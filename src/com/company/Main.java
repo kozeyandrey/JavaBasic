523b47fd4e7aebd5fc877578fb5190ab54ccf1e9
Andrew Kozey <kozeyandrey@gmail.com>
2017-01-31 23:36:47 +0200

    Introduce in variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String myName = "Andrew";
+	    float favoriteNumber = 7;
+	    System.out.println(myName + "'s favorite number is " + favoriteNumber);
+    }
+}
