f05033a0396e3fb6ade64fdc5abbd0762ba42344
Andrew Kozey <kozeyandrey@gmail.com>
2017-02-01 14:51:18 +0200

    Introduce in input and output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MyClass mc = new MyClass();
-        System.out.println(mc.myNum);
-        mc.myMethod();
-	}
+        System.out.println("Enter first number:");
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = scan.nextInt();
+        System.out.println("Sum is " + (num1 + num2));
+    }
 }
